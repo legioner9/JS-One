58a20a0d05d82b75a34b662f743e9a11fcf33889
Legioner9 <legioner9@inbox.ru>
2019-04-14 02:21:45 +0700

    s

--- app/js/script.js
+++ app/js/script.js
@@ -8,7 +8,7 @@ var COLOR_GREEN = '#F00';
 var admin,name;
 name = 'Vasya';
 admin = name ;
-console.log(admin);
+// console.log(admin);
 // number n=12.56
 // Infinity alert(1/0)
 // NaN alert('not number'/7)
@@ -18,4 +18,22 @@ console.log(admin);
 // undefined
 // obj 
 var user ={name:'Petya'};
-cl(typeof user);
+// cl(typeof user);
+// string to string only (+) :  1 + '2' -- '12'
+// 2 - '1' -- 1
+// 4/'2'  --  2
+// var n = '12', surn = '34'
+// n + surn  -- '1234'
+// +n + +surn -- 46
+var a = 1 , b = 2 , c = 3 - (a = b + 1);
+cl('c = ' + c);
+cl(5%2);
+cl('2'>'1444');
+cl(parseInt('11000', 2));
+var n = 2;
+cl(n.toString(2));
+n = 9 ^ 35 ;
+cl('9 ^ 35 = '+n);
+cl(n.toString(2));
+n = n ^ 35 ;
+cl('n ^ 35 = '+n);
