f758ad53b982a7e2e52cf9725fa2585b906fb041
Legioner9 <legioner9@inbox.ru>
2019-04-13 23:56:44 +0700

    t

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,21 @@
+function cl(volume){
+    console.log(volume)
+};
+
+
+
 var COLOR_GREEN = '#F00';
 var admin,name;
 name = 'Vasya';
 admin = name ;
 console.log(admin);
+// number n=12.56
+// Infinity alert(1/0)
+// NaN alert('not number'/7)
+// sting 'My skript'
+// bolean false true
+// null
+// undefined
+// obj 
+var user ={name:'Petya'};
+cl(typeof user);
