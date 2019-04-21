4cdadd32c9ead413a58533e95f8cc02f311f8976
Legioner9 <legioner9@inbox.ru>
2019-04-21 13:32:27 +0700

    6

--- app/js/script.1.js
+++ app/js/script.1.js
@@ -1,3 +1,7 @@
+// import { isPromiseAlike } from "q";
+
+// import { isPromise } from "q";
+
 function col(volume){
     console.log(volume);
 }
@@ -7,6 +11,38 @@ function isInt(volume){
 }
 
 
-col('ruiop66'.length);
-col('ruiop66'.length);
-col('ruiop66'.length);
+// col('ruiop66'.length);
+// var t = 66;
+// col(t+5);
+// var f = function rits(name){
+//     var z = 5;
+//     col(rits);
+// };
+
+// f();
+// var g = f;
+// f = null;
+// col(g);
+// var na = prompt('you name is ...','you name');
+// col(na);
+// col(confirm('message'));
+
+// console.dir(document.head);
+// console.dirxml(document);
+
+function showPrime(n){
+    for(var i=2 ; i<n ; i++){
+        if(!isPrime(i)) continue;
+        col(i);
+    }
+}
+
+function isPrime(n){
+    for (var i=2 ; i<n ; i++){
+        if ( n%i == 0) return false;
+    }
+    return true;
+}
+
+// showPrime(12);
+col(isPrime(4));
