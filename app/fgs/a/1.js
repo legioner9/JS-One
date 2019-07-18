b401433484202d41792c1b965c9800b114fc43c4
Legioner9 <legioner9@inbox.ru>
2019-07-18 18:48:06 +0700

    151 comm

---
+++ app/fgs/a/1.js
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2019. Legioner9@inbox.ru
+ */
+debugger;
+col(location.href);
+document.body.style.background = 'red';
+col( 'Элемент BODY стал красным, а сейчас обратно вернётся' );
+document.body.style.background = '';
